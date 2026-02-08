39c36a784089e7baf889cfc841b6000a5ca27ba7
aliona matveeva <yellalena@gmail.com>
2026-02-08 16:59:01 +0100

    updated vkscape version

--- Formula/vkscape.rb
+++ Formula/vkscape.rb
@@ -1,7 +1,7 @@
 class Vkscape < Formula
   desc "CLI/TUI tool for downloading your VK archive"
   homepage "https://github.com/yellalena/vkscape"
-  url "https://github.com/yellalena/vkscape/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/yellalena/vkscape/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "125a2c0f8a4c7c9092bd736411986f670a7188b5e9504b4107ee122f839ca791"
   license ""
 

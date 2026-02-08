e0bbfa9196d39c087c48f882aab41ecbbda5f016
aliona matveeva <yellalena@gmail.com>
2026-02-08 16:15:35 +0100

    added vkscape formula

---
+++ Formula/vkscape.rb
@@ -0,0 +1,19 @@
+class Vkscape < Formula
+  desc "CLI/TUI tool for downloading your VK archive"
+  homepage "https://github.com/yellalena/vkscape"
+  url "https://github.com/yellalena/vkscape/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "125a2c0f8a4c7c9092bd736411986f670a7188b5e9504b4107ee122f839ca791"
+  license ""
+
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"vkscape", "."
+  end
+
+  test do
+    system "#{bin}/vkscape", "--help"
+  end
+end
